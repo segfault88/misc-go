3376704825d69f5a1045f8dd2aee2addd5cb7394
Malcolm Lockyer <segfault88@gmail.com>
2015-08-10 20:39:02 +1200

    add code for wg-gotcha post

---
+++ wg-gotcha-fixed.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go doWork(i, &wg)
+	}
+
+	log.Println("Starting to wait")
+	wg.Wait()
+	log.Println("Done")
+}
+
+func doWork(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	sleepTime := time.Duration(rand.Int31n(10000000))
+	time.Sleep(sleepTime)
+	log.Printf("%d slept for %d", i, sleepTime)
+}
